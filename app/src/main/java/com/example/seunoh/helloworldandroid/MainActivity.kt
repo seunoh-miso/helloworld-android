f145485e9f2662180f8123bd1079fdca8d74d58e
Seun Oh <seunoh@getmiso.com>
2018-01-17 20:56:20 +0900

    Add Random color

--- app/src/main/java/com/example/seunoh/helloworldandroid/MainActivity.kt
+++ app/src/main/java/com/example/seunoh/helloworldandroid/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.seunoh.helloworldandroid
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,11 +13,21 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.view_text)
     }
 
+    private val view by lazy {
+        findViewById<View>(R.id.container)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView.text = "Hello World! Kotlin in native!"
+        val random = Random()
+        val bgColor = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+        view.setBackgroundColor(bgColor)
+        textView.setTextColor(Color.rgb(255 - Color.red(bgColor),
+                255 - Color.green(bgColor),
+                255 - Color.blue(bgColor)))
     }
 
     override fun onResume() {
